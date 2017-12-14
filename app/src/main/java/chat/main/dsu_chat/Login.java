5ab747d9a0969ef0da2fa0aa79971bf41552b43e
Infant Derrick Gnanasusairaj <infant.derrick.gnanasusairaj@gmail.com>
2017-12-14 09:27:21 +0530

    Firebase Config

---
+++ app/src/main/java/chat/main/dsu_chat/Login.java
@@ -0,0 +1,17 @@
+package chat.main.dsu_chat;
+
+import android.app.Activity;
+import android.widget.Button;
+import android.widget.EditText;
+
+/**
+ * Created by infan_000 on 12/14/2017.
+ */
+
+
+public class Login extends Activity {
+    private EditText username;
+    private EditText password;
+    private Button login;
+
+}
