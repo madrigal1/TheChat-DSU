6f951e60734e8b41abbf590467386cd98be145de
Infant Derrick Gnanasusairaj <infant.derrick.gnanasusairaj@gmail.com>
2017-12-14 10:28:23 +0530

    Firebase SHeese

--- app/src/main/java/chat/main/dsu_chat/Login.java
+++ app/src/main/java/chat/main/dsu_chat/Login.java
@@ -1,12 +1,19 @@
 package chat.main.dsu_chat;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 /**
@@ -29,5 +36,41 @@ public class Login extends Activity {
         password = findViewById(R.id.password);
         login = findViewById(R.id.login);
         mref = FirebaseDatabase.getInstance().getReference();
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mref.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if(dataSnapshot.hasChild(username.getText().toString())){
+                            mref.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(DataSnapshot dataSnapshot){
+                                    Users user = dataSnapshot.getValue(Users.class);
+                                    if(password.getText().toString() == user.password){
+                                        Intent intent = new Intent(Login.this, MainActivity.class);
+                                        intent.putExtra("user", username.getText().toString());
+                                    }else{
+                                        Toast.makeText(Login.this, "Invalid Login!", Toast.LENGTH_LONG).show();
+                                    }
+
+                                }
+
+                                @Override
+                                public void onCancelled(DatabaseError databaseError) {
+                                    Toast.makeText(Login.this, "Database Error", Toast.LENGTH_LONG).show();
+                                }
+                            });
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
     }
 }
---
+++ app/src/main/java/chat/main/dsu_chat/Users.java
@@ -0,0 +1,16 @@
+package chat.main.dsu_chat;
+
+/**
+ * Created by infan_000 on 12/14/2017.
+ */
+
+public class Users {
+    public String password;
+    public String name;
+    public String email;
+    public String photo;
+
+    public Users(){
+        //constructers
+    }
+}
