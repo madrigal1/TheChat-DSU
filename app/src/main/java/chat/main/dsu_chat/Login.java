b4bca879df43b8de21de271c868d7e90122f1950
Infant Derrick Gnanasusairaj <infant.derrick.gnanasusairaj@gmail.com>
2017-12-14 09:55:55 +0530

    FUCK THAT SHIT

--- app/src/main/java/chat/main/dsu_chat/Login.java
+++ app/src/main/java/chat/main/dsu_chat/Login.java
@@ -1,8 +1,13 @@
 package chat.main.dsu_chat;
 
 import android.app.Activity;
+import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 /**
  * Created by infan_000 on 12/14/2017.
@@ -13,5 +18,16 @@ public class Login extends Activity {
     private EditText username;
     private EditText password;
     private Button login;
+    private DatabaseReference mref;
 
+
+    @Override
+    public void onCreate(final Bundle savedState) {
+        super.onCreate(savedState);
+        setContentView(R.layout.simple_login);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        login = findViewById(R.id.login);
+        mref = FirebaseDatabase.getInstance().getReference();
+    }
 }
